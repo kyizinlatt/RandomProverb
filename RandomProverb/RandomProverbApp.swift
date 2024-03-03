c753b7c1847365fa6de608a328c2f8eb12f442da
Kyi Zin Latt <kyizinlatt@gmail.com>
2024-03-03 09:49:21 +0700

    Initial Commit

---
+++ RandomProverb/RandomProverbApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomProverbApp.swift
+//  RandomProverb
+//
+//  Created by Kyi Zin Latt on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomProverbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
